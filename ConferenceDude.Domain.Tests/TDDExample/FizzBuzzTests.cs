8d1d2289864809a7cfea634546727d3d74784683
Andreas Richter <ar@anrichter.net>
2020-10-30 11:55:28 +0100

    TDD Beispiel (FizzBuzz)

---
+++ ConferenceDude.Domain.Tests/TDDExample/FizzBuzzTests.cs
@@ -0,0 +1,50 @@
+﻿using ConferenceDude.Domain.TDDExample;
+using NUnit.Framework;
+
+namespace ConferenceDude.Domain.Tests.TDDExample
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(4, "4")]
+        [TestCase(98, "98")]
+        public void FizzBuzz_mit_natuerlichen_Zahlen(int zahl, string expected)
+        {
+            var sut = new FizzBuzz();
+            var actual = sut.ToFizzBuzz(zahl);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase("3", "Fizz")]
+        [TestCase("6", "Fizz")]
+        [TestCase("9", "Fizz")]
+        [TestCase("99", "Fizz")]
+        public void FizzBuz_mit_Zahl_durch_drei_teilbar(int zahl, string expected)
+        {
+            var sut = new FizzBuzz();
+            var actual = sut.ToFizzBuzz(zahl);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase(5, "Buzz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(20, "Buzz")]
+        public void FizzBuzz_mit_Zahl_durch_fuenf_teilbar(int zahl, string expected)
+        {
+            var sut = new FizzBuzz();
+            var actual = sut.ToFizzBuzz(zahl);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(30, "FizzBuzz")]
+        public void FizzBuzz_mit_Zahl_druch_drei_und_fuenf_teilbar(int zahl, string expected)
+        {
+            var sut = new FizzBuzz();
+            var actual = sut.ToFizzBuzz(zahl);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ ConferenceDude.Domain/TDDExample/FizzBuzz.cs
@@ -0,0 +1,41 @@
+﻿namespace ConferenceDude.Domain.TDDExample
+{
+    public class FizzBuzz
+    {
+        public string ToFizzBuzz(int zahl)
+        {
+            var result = string.Empty;
+
+            if (zahl % 3 == 0)
+            {
+                result = "Fizz";
+            }
+
+            if (zahl % 5 == 0)
+            {
+                result += "Buzz";
+            }
+
+            if (string.IsNullOrEmpty(result))
+            {
+                result = zahl.ToString();
+            }
+
+
+            //if (zahl % 15 == 0)
+            //{
+            //    return "FizzBuzz";
+            //}
+            //if (zahl % 5 == 0)
+            //{
+            //    return "Buzz";
+            //}
+            //if (zahl % 3 == 0)
+            //{
+            //    return "Fizz";
+            //}
+
+            return result;
+        }
+    }
+}
